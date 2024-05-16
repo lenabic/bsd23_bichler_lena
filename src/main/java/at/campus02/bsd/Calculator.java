92dbe392a4213b3fb0543d8ef0766e36d40149dd
Lena Bichler <12032376@edu.campus02.at>
2024-05-16 18:28:27 +0200

    Add screenshot of java versions and mvn

--- src/main/java/at/campus02/bsd/Calculator.java
+++ src/main/java/at/campus02/bsd/Calculator.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class Calculator {
+package at.campus02.bsd;
+
+public class Calculator {
+    public double add(double number1, double number2) {
+        return number1 + number2;
+    }
+
+    public double minus(double number1, double number2) {
+        return number1 - number2;
+    }
+
+    public double divide(double number1, double number2) {
+        return number1 / number2;
+    }
+
+    public double multiply(double number1, double number2) {
+        return number1 * number2;
+    }
 }
+

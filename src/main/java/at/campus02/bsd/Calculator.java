b33226377418b9d8251ce817f294426c8eab16f2
Lena Bichler <12032376@edu.campus02.at>
2024-05-31 00:16:58 +0200

    making sure division with 0 equals an Exception

--- src/main/java/at/campus02/bsd/Calculator.java
+++ src/main/java/at/campus02/bsd/Calculator.java
@@ -10,7 +10,11 @@ public class Calculator {
     }
 
     public double divide(double number1, double number2) {
-        return number1 / number2;
+        if (number2 == 0) {
+            throw new ArithmeticException("Division by zero is not allowed");
+        } else {
+            return number1 / number2;
+        }
     }
 
     public double multiply(double number1, double number2) {

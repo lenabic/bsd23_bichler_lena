b6fc8a31c4c16b7ea98863bb4f2895509b015352
Lena Bichler <12032376@edu.campus02.at>
2024-06-19 23:57:04 +0200

    include logging in the Calculator class

--- src/main/java/at/campus02/bsd/Calculator.java
+++ src/main/java/at/campus02/bsd/Calculator.java
@@ -1,23 +1,33 @@
 package at.campus02.bsd;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class Calculator {
+    private static Logger logger = LogManager.getLogger(Calculator.class);
+
     public double add(double number1, double number2) {
+        logger.debug("Adding {} and {}", number1, number2);
         return number1 + number2;
     }
 
     public double minus(double number1, double number2) {
+        logger.debug("Subtracting {} from {}", number2, number1);
         return number1 - number2;
     }
 
     public double divide(double number1, double number2) {
+        logger.debug("Dividing {} by {}", number1, number2);
         if (number2 == 0) {
-            throw new ArithmeticException("Division by zero is not allowed");
+            logger.error("Number 2 cannot be zero");
+            throw new ArithmeticException();
         } else {
             return number1 / number2;
         }
     }
 
     public double multiply(double number1, double number2) {
+        logger.debug("Multiplying {} with {}", number1, number2);
         return number1 * number2;
     }
 
